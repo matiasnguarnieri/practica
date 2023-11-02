477bce15ef26960541e9ee26edc5d1760091497d
Mati <matiasnguarnieri@hotmail.com>
2023-11-02 19:18:04 -0300

    prueba2

---
+++ test/unlam/edu/ar/TestPanini.java
@@ -0,0 +1,14 @@
+package unlam.edu.ar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPanini {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
